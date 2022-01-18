baa0af4de73f3c548907ec4090b46486a661f88a
Maheswar Nagella <maheswar.nagella@gmail.com>
2022-01-18 16:16:45 -0500

    Added HelloWorld file

---
+++ src/com/nagella/core/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.nagella.core;
+
+public class HelloWorld {
+    public static void main(String args[]){
+        
+        System.out.println("Welcome to Core Java Interview Coding Questions");
+    }
+}
